cd4c9a1058522f9042313a1003e0f69f7f64b21a
Kasperi Vaarala <n1vaka00@students.oamk.fi>
2022-01-06 22:59:54 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,23 @@
+// lets createe a HTTP request
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "https://yesno.wtf/api",true);
+// send request
+xmlhttp.send();
+
+//create an event handler
+xmlhttp.onreadystatechange=function() {
+    if(xmlhttp.readyState === 4 && xmlhttp.status === 200)
+    {
+
+    
+  }
+}
+// added button function
+function yesFunction(){
+    let answerObject = JSON.parse(xmlhttp.response)
+    //call answer
+    document.getElementById("answer").innerHTML = answerObject.answer;
+    //call image
+    document.getElementById("answer_image").setAttribute('src', answerObject.image);
+}
+
